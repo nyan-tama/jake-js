725aaeee33fc2390eb3ef1352be500358e6be407
nyan-tama <shake.flake.love@gmail.com>
2023-02-07 00:16:58 +0900

    add 仮クリックイベント実装

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 const onClickAdd = () => {
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value = "";
-    alert(inputText)
+
+    const ul = document.createElement("ul");
+    ul.className = "list-row"
+
+    const li = document.createElement("li");
+    li.innerText = inputText;
+
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => onClickAdd());
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    deleteButton.addEventListener("click", () => onClickAdd());
+
+    ul.appendChild(li);
+    ul.appendChild(completeButton);
+    ul.appendChild(deleteButton);
+    document.getElementById("imcomplete-list").appendChild(ul);
 };
 
 document.getElementById("add-button").addEventListener("click", () => onClickAdd());
