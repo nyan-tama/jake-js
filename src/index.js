dd38a99348f3027a52330ec737ce58a22cf5cf7a
nyan-tama <shake.flake.love@gmail.com>
2023-02-06 23:53:47 +0900

    add 途中まで

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const onClickAdd = () => {
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+    alert(inputText)
+};
+
+document.getElementById("add-button").addEventListener("click", () => onClickAdd());
