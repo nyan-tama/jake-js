824c04f2feac2e2fedc436c6ab960e9789e4b4a9
nyan-tama <shake.flake.love@gmail.com>
2023-02-07 11:24:02 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -1,67 +1,63 @@
-
+//入力フォーム
 const onClickAdd = () => {
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value = "";
 
-    createImpcompleteList(inputText);
+    createIncompleteList(inputText);
 };
 
-
-document.getElementById("add-button").addEventListener("click", () => onClickAdd());
-
-const deleteFromImcompleteList = (target) => {
-    document.getElementById("imcomplete-list").removeChild(target);
+const deleteFromIncompleteList = (target) => {
+    document.getElementById("incomplete-list").removeChild(target);
 }
 
 
-const createImpcompleteList = (text) => {
-    const ul = document.createElement("ul");
-    ul.className = "list-row"
+const createIncompleteList = (text) => {
+    const div = document.createElement("div");
+    div.className = "list-row"
 
     const li = document.createElement("li");
-    li.innerText = inputText;
+    li.innerText = text;
 
     const completeButton = document.createElement("button");
     completeButton.innerText = "完了";
     completeButton.addEventListener("click", () => {
-        const completeTarget = completeButton.parentNode;
-        deleteFromImcompleteList(completeTarget);
+        deleteFromIncompleteList(completeButton.parentNode);
 
         const addTarget = completeButton.parentNode;
         const text = addTarget.firstElementChild.innerText;
 
         addTarget.textContent = null;
-
-        const li = document.createElement("li");
         li.innerText = text;
 
         const backButton = document.createElement("button");
         backButton.innerText = "戻す";
         backButton.addEventListener("click", () => {
-            const deleteTarget = backButton.parentNode;
+            deleteTarget = backButton.parentNode;
             document.getElementById("complete-list").removeChild(deleteTarget);
 
             const text = backButton.parentNode.firstElementChild.innerText;
-            console.log(text);
+            // 再帰
+            createIncompleteList(text);
         });
 
         addTarget.appendChild(li);
         addTarget.appendChild(backButton);
 
-
         document.getElementById("complete-list").appendChild(addTarget);
-
-    });
+    })
 
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "削除";
     deleteButton.addEventListener("click", () => {
-        const deleteTarget = deleteButton.parentNode;
-        deleteFromImcompleteList(deleteTarget);
-    });
+        deleteFromIncompleteList(deleteButton.parentNode);
+    })
 
-    ul.appendChild(li);
-    ul.appendChild(completeButton);
-    ul.appendChild(deleteButton);
-    document.getElementById("imcomplete-list").appendChild(ul);
+    div.appendChild(li);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
+
+    document.getElementById("incomplete-list").appendChild(div);
 }
+
+//イベントリスナー
+document.getElementById("add-button").addEventListener("click", () => onClickAdd());
